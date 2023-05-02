6b43aa30812e1278ff4468d4851d3a3c2b2c6d8f
lorossi <mail@lorenzoros.si>
2023-05-02 10:47:20 +0200

    updated build script to test installation

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# activate virtual environment
-source venv/bin/activate
 # run all test, pipe all output to stdout and grep for the OK string
 result=$(python3 -m unittest discover -s ./tests -p "test*.py" -q 2>&1 | grep OK)
 
@@ -19,27 +17,56 @@ docsfolder="docs"
 jsonreport="coverage.json"
 buildsfolder="dist"
 
-# compute the coverage and pipe all output to /dev/null
+# activate virtual environment
+source venv/bin/activate
+# compute the coverage
 coverage run -m unittest discover -s ./tests -p "test*.py" -q
 echo -e "\033[1;32mCoverage computed and saved in $reportfolder\033[0m"
+# generate the coverage report in html
 coverage html -d $reportfolder
 echo -e "\033[1;32mCoverage report generated in $reportfolder\033[0m"
-# generate the coverage as a json file
+# generate the coverage report in json
 coverage json -o $jsonreport
 echo -e "\033[1;32mJSON file generated in $jsonreport\033[0m"
 # build the documentation
 cd src
 pdoc --html -o ../$docsfolder customdataclass.py
 echo -e "\033[1;32mDocumentation generated in $docsfolder\033[0m"
-# rename the file
+# rename the file so it is currently displayed in gihub pages
 mv ../$docsfolder/customdataclass.html ../$docsfolder/index.html
 # return to the root folder
 cd ..
 # clean the build folder
 rm -rf $buildsfolder
-# build the package and get the last word as the package name
+# build the package
 python3 -m build -o $buildsfolder
 echo -e "\033[1;Builds saved in $buildsfolder\033[0m"
+# zip the builds
+tar -czvf $buildsfolder.tar.gz $buildsfolder
+echo -e "\033[1;32mBuilds zipped in $buildsfolder.tar.gz\033[0m"
+# deactivate virtual environment
+deactivate
+
+# try to install the package
+echo -e "\033[1;32mInstalling the package\033[0m"
+# create a virtual environment
+python3 -m venv buildenv
+# activate the virtual environment
+source buildenv/bin/activate
+# install the requirements
+pip3 install PyYAML toml
+# find the package name
+package=$(ls $buildsfolder | grep .whl)
+# install the package, if it fails, exit
+pip3 install $buildsfolder/$package
+# deactivate the virtual environment
+deactivate
+if [ $? -ne 0 ]
+then
+    echo -e "\033[1;31mInstallation failed. Exiting.\033[0m"
+    exit 1
+fi
+
 
 # ask the user if he wants to push the package to pypi (in yellow)
 read -p $'\033[1;33mDo you want to push the package to PyPi? (y/n) \033[0m' -n 1 -r
@@ -52,9 +79,17 @@ then
     exit 1
 fi
 
+# create a virtual environment for the build
+python3 -m venv releaseenv
+# activate the virtual environment
+source releaseenv/bin/activate
+# install the requirements
+pip install twine
 # install twine
 echo -e "\033[1;32mInstalling twine\033[0m"
 pip3 install twine
 # push the page to pypi using twine
 echo -e "\033[1;32mPushing to PyPi\033[0m"
 twine upload $buildsfolder/*
+# deactivate the virtual environment
+deactivate
